ae709d465c90297570794769d202b3130f83e25c
MatthewM5293 <mmunozramirez@student.neumont.edu>
2023-01-20 15:31:55 -0700

    Working on formatting

--- VGLibrary/Controllers/HomeController.cs
+++ VGLibrary/Controllers/HomeController.cs
@@ -8,6 +8,16 @@ namespace VGLibrary.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        //hardcoded game list
+        private static List<Game> Gamelist = new List<Game>
+        {
+            new Game(1, "Gears Of War 3", "Xbox 360", "3rd Person", "Mature", 2011, "GearsOfWar3.jpg", null, null),
+            new Game(2, "Halo Infinite", "Xbox", "1st Person Shooter", "Teen", 2021, "Halo_Infinite.png", null, null),
+            new Game(3, "Doki Doki Literature Club", "PC", "Visual Novel", "Mature", 2017, "DokiDoki.jpg", null, null),
+            new Game(4, "Minecraft: Java Edition", "PC", "Sandbox", "PC", 2009, "Minecraft.jpg", null, null),
+            new Game(5, "Fortnite", "PC", "3rd Person", "Teen", 2017, "Fortnite.jpg", null, null)
+        };
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -20,7 +30,7 @@ namespace VGLibrary.Controllers
         
         public IActionResult Collection()
         {
-            return View();
+            return View(Gamelist);
         }
 
         public IActionResult Privacy()
--- VGLibrary/Models/Game.cs
+++ VGLibrary/Models/Game.cs
@@ -6,16 +6,29 @@
         public int? Id { get; set; } = nextID++;
 
         public string Title { get; set; } = "{NO TITLE}";
-        public string Platform { get; set; } = "{NO PLATFORM}";
+        public string? Platform { get; set; } = "{NO PLATFORM}";
         public string Genre { get; set; }
 
-        public float? Rating { get; set; } = 0f;
-        public int? Year { get; set; } = 1958;
+        public string? Rating { get; set; }
+        public int Year { get; set; } = 1958;
         public string Image { get; set; }
-        public string LoanedTo { get; set; }
+        public string? LoanedTo { get; set; }
 
 
         public DateTime? LoanDate { get; set; }
 
+        public Game() { }
+        public Game(int? id, string title, string? platform, string genre, string? ESRBrating, int year, string image, string? loanedTo, DateTime? loanDate)
+        {
+            Id = id;
+            Title = title;
+            Platform = platform;
+            Genre = genre;
+            Rating = ESRBrating;
+            Year = year;
+            Image = image;
+            LoanedTo = loanedTo;
+            LoanDate = loanDate;
+        }
     }
 }
